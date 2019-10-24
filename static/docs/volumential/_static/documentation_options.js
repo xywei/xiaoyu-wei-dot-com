a922d3711dfe6df8059ff9e861a897b9028b15d1
Volumential Documentation CI Builder <xywei@pm.me>
2019-10-24 13:52:09 -0500

    Auto-updated volumential docs

--- static/docs/volumential/_static/documentation_options.js
+++ static/docs/volumential/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2017.1',
+    VERSION: '2017.1alpha',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
